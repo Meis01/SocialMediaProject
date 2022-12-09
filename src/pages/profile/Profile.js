5d01efa3825bb056cfac327519c3c71158941bef
Meis01 <meis.baas95@hotmail.com>
2022-12-09 16:46:51 +0300

    creating profile page

--- src/pages/profile/Profile.js
+++ src/pages/profile/Profile.js
@@ -1,14 +1,20 @@
-import Main from '../../components/main/Main';
+import Main from "../../components/main/Main";
+import "./Profile.css";
+import React, { useState } from "react";
 
 const Profile = () => {
-
-    return(
-        <>
-        <Main title='Profile'>
-
-        </Main>
-        </>
-    )
-}
+  const [image, setImage] = useState();
+  function handleChange(e) {
+    setImage(URL.createObjectURL(e.target.files[0]));
+  }
+  return (
+    <>
+      <Main title="Profile">
+        <input type="file" onChange={handleChange} />
+        <img src={image} />
+      </Main>
+    </>
+  );
+};
 
 export default Profile;
